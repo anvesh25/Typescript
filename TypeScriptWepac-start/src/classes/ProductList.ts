7b66a265ca49bfbe93d8ca1ffde6afaf05199c75
slk <slk@WSLK1GFTR4P-09.SLKSOFT.COM>
2018-06-09 16:45:47 +0530

    commit

--- TypeScriptWepac-start/src/classes/ProductList.ts
+++ TypeScriptWepac-start/src/classes/ProductList.ts
@@ -1,3 +1,4 @@
+import * as $ from 'jquery';
 import {Iproducts} from '../models/product';
 import {Icartitem} from '../models/cartitem';
 import {cartitem} from './/cart';
@@ -7,14 +8,19 @@ export class productlist
     constructor(private list:Array<Iproducts>, public cart: cartitem)
     {
         this.productlistcontainer =document.querySelector('.product-list') as HTMLDivElement 
+        //this.productlistcontainer =$('.product-list') as JQuery<HTMLDivElement>;
     }
     renderlist(): void 
     {
         this.list.forEach((item: Iproducts) => 
         {
+            //const productDivJqueryEl: JQuery<HTMLDivElement> = $('<div />') as JQuery<HTMLDivElement>;
             const productDivEl:HTMLDivElement=document.createElement('div');
             productDivEl.className ='product-item';
             const productTitleEl : HTMLHeadingElement =document.createElement('h3') as HTMLHeadingElement;
+            const className ='col-12 col-sm-6 product-item';
+            //const productImageEl : HTMLImageElement =document.createElement('img') as HTMLImageElement;
+            //Jquery changes
             const productImageEl : HTMLImageElement =document.createElement('img') as HTMLImageElement;
             const ProductPriceEl :HTMLParagraphElement =document.createElement('Price') as HTMLParagraphElement
             const productbuttonEl : HTMLButtonElement =document.createElement('button') as HTMLButtonElement;
@@ -22,8 +28,11 @@ export class productlist
 
             productTitleEl.textContent = item.title;
             productImageEl.src =item.ImageUrl;
+            productImageEl.className="img-fluid rounded";
             ProductPriceEl.textContent=item.Price.toString();
             productbuttonEl.textContent =" Add to Cart";
+            productbuttonEl.className="btn btn-success";
+
             productbuttonEl.addEventListener('click', () => 
             {
                 this.cart.addtocart(item.id);
